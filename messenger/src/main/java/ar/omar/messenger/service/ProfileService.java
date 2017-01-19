676fb333844ecdd98acac364ea33bd372a0a3f2d
Omar Aguirre <omar.aguirre.1984@gmail.com>
2017-01-19 13:53:04 -0300

    primer commit de messenger

---
+++ messenger/src/main/java/ar/omar/messenger/service/ProfileService.java
@@ -0,0 +1,52 @@
+package ar.omar.messenger.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import ar.omar.messenger.database.DatabaseClass;
+import ar.omar.messenger.model.Profile;
+
+public class ProfileService {
+	
+	
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	
+	public ProfileService() {
+		profiles.put("Omar", new Profile(1L, "omar", "Omar", "Ag", new Date()));
+		profiles.put("Armando", new Profile(2L, "armando", "Armando", "Ag", new Date()));
+	}
+
+	public Profile getProfile(String profileName){
+		return profiles.get(profileName);
+	}
+	
+	public Profile addProfile(Profile profile){
+		profile.setId(profiles.size()+1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile){
+		Profile res;
+		
+		if (profile.getId() <= 0) {
+			res= null;
+		}else{
+			profiles.put(profile.getProfileName(), profile);
+			res = profile;
+		}
+		return res;
+	}
+	
+	public Profile removeProfile(String profileName){
+		return profiles.remove(profileName);
+	}
+	
+	
+	public List<Profile> getAllProfiles(){
+		return new ArrayList<Profile>(profiles.values());
+	}
+}
