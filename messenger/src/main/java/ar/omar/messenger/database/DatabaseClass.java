58b6485abd84ec3dc5cfcd66a19fa80a30ae7b41
Omar Aguirre <omar.aguirre.1984@gmail.com>
2017-01-20 21:57:35 -0300

    x

--- messenger/src/main/java/ar/omar/messenger/database/DatabaseClass.java
+++ messenger/src/main/java/ar/omar/messenger/database/DatabaseClass.java
@@ -11,8 +11,6 @@ public class DatabaseClass {
 	private static Map<Long, Message> messages = new HashMap<>();
 	private static Map<String, Profile> profiles = new HashMap<>();
 	
-
-	
 	
 	public static Map<Long, Message> getMessages() {
 		return messages;
@@ -21,6 +19,3 @@ public class DatabaseClass {
 		return profiles;
 	}
 }
-	
-	
-	
